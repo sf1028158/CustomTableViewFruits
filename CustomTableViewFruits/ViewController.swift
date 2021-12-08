3be34e415069394e76d0f2d6cb6fe9bccfcfd566
Fang Shao <shao.fa@northeastern.edu>
2021-12-07 21:13:51 -0800

    Initial Commit

---
+++ CustomTableViewFruits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomTableViewFruits
+//
+//  Created by Fang Shao on 12/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
